8866e32af3b963c7bcbc4bb3ff60313d02768071
Cathrine Hertenstein <chertenstein@berklee.edu>
2019-04-01 15:46:37 -0400

    Assignment 8 try 3

--- Assignment8.js
+++ Assignment8.js
@@ -1,20 +1,25 @@
 class MySet {
   constructor(valueArray){
-    this._set1 = valueArray;
+    this._set1 = [];
     }
-    addNumber(){
-      this._set1.push();
+    addNumber(value){
+      this._set1.push(value);
     }
-    removeNumber(){
-      this._set1.remove();
+    removeNubmer(value){
+      this._set1.remove(value);
     }
+}
+function removeNumber(value){
+  this._set1.removeNumber(value);
 }
 let my_set = new MySet([1, 2, 3, 4]);
 let my_set2 = new MySet([5, 6, 7, 8]);
 let final_set = new MySet();
-final_set.addNumber('9');
-final_set.removeNumber('4');
-
+final_set.addNumber([9]);
+// final_set.removeNumber([4]);
+console.log(my_set);
+console.log(my_set2);
+console.log(final_set);
 function test_set(final_set, newValue){
   for(let i = 0; i<= final_set.length; i++){
     if(final_set[i] == newValue){
